87c4a6a7552b2eb3b3c530e22400b875e73a2ca4
MoonieSof <mooniesofi@gmail.com>
2025-11-07 15:31:20 -0600

    Examen2doP: commit final

--- Examen2doP/App.js
+++ Examen2doP/App.js
@@ -1,16 +1,16 @@
 import{useEffect,useState} from 'react';
-import {Text,styleSheet,View,SafeAreaView,ImageBackground} from 'react-native'
+import {Text,StyleSheet,View,SafeAreaView,ImageBackground} from 'react-native'
 
 export default function App(){
 
 const [showSplash,setShowSplash]= useState(true)
 
 useEffect ( ()=>{
-  const timer =setTimeout( () =>{
+  const timer =setTimeout(() =>{
     setShowSplash(false);
 
-  }, 3000);
-  return ()=> clearTimeout(timer);
+  }, 2000);
+  return () => clearTimeout(timer);
 
 },[]);
 
@@ -22,15 +22,47 @@ if (showSplash){
 
       <Text style={styles.splashText}>   Bienvenido a mi perfil  </Text> 
     </View>
+
   );
 }
 
+return (
+
+     <SafeAreaView style={{flex: 1}}>
+
+    <ImageBackground 
+
+    source ={{
+        uri:'https://www.patasencasa.com/sites/default/files/styles/article_detail_1200/public/2024-09/hello-kitty-historia_0.jpg.webp?itok=vIKuhqYw',
+    }}
+
+style={styles.background}
+>
+<View style={styles.overlay} >  
+
+ 
+
+  <Text style = {styles.title}>
+            Lopez vazquez Ana sofia 
+           estudiante de ITIID 
+           Naci el 8 de mayo de 2005,Tengo 20 años.
+           124049444@upq.edu.mx
+           4422082724    
+   </Text>
+   
 
 
+</View>  
+
+</ImageBackground>
+
+</SafeAreaView>
+
+);
 
 }
 
-const styles = styleSheet.create({
+const styles = StyleSheet.create({
 
 splashContainer:{
 
@@ -47,13 +79,28 @@ splashText:{
 
 },
 
+background:{
+
+  flex:1,
+  justifyContent:'center',
+  alignItems:'center'
+},
+
+overlay:{
+  backgroundColor:'# rgba(0,0,0,0.5)',
+  padding:20,
+  borderRadius:10,
+},
+
+title:{
+  fontSize:28,
+  color:'#fff',
+  marginBottom:10,
+  textAlign:'center',
 
 
 
-
-
-
-
+},
 
 
 });
